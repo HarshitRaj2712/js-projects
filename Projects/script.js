a9eaf94e9faf76a7038d1352590e025d0abb5e08
HarshitRaj2712 <panditjee2712@gmail.com>
2026-02-04 10:32:59 +0530

    final

---
+++ Projects/script.js
@@ -0,0 +1,16 @@
+let x = document.querySelector('#emoji-container');
+let y = document.querySelector('#btn');
+
+const e = [
+    '😀','😂','😍','🤔','😎','😭','😡','👍','🙏','🎉',
+    '💔','🔥','😶‍🌫️','💀','🦴','🤡','🚀','👑','🌈','⚡','🎯','🧠','👀','😴','🥶','🥵',
+    '🤯','😇','🫶','💎','🕶️','🎧','🍕','🌍','🐉','🦄'
+];
+
+y.addEventListener('click', () => {
+    x.textContent = set();
+});
+
+let set = () => {
+    return e[Math.floor(Math.random() * e.length)];
+};
