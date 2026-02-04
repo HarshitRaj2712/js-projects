421c365a5edcd13298b8100f8c48b41959b029be
HarshitRaj2712 <panditjee2712@gmail.com>
2026-02-04 09:58:46 +0530

    final change

--- Projects/01-project.js
+++ Projects/01-project.js
@@ -1,8 +1,12 @@
 let x=document.querySelector('#emoji-container');
 let y=document.querySelector('#btn');
 
-const e=['😀','😂','😍','🤔','😎','😭','😡','👍','🙏','🎉','💔','🔥'];
+const e=['😀','😂','😍','🤔','😎','😭','😡','👍','🙏','🎉','💔','🔥','😶‍🌫️','💀','🦴','🤡'];
 
 y.addEventListener('click',()=>{
-    let x.value=Math.floor(Math.random()*e.length);
+     x.textContent=set();
 })
+
+let set=()=>{
+    return e[Math.floor(Math.random()*e.length)];
+}
